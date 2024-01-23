0d1cfe85660a792d6b69f2534cece352605de6c2
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-01-23 12:30:09 +0500

    Track Player functionality added

---
+++ src/constants.ts
@@ -0,0 +1,46 @@
+import {Track} from 'react-native-track-player';
+
+export const playListData: Track[] = [
+  {
+    id: 1,
+    title: 'Surah Yaseen',
+    artist: 'Aaar Al Hudhoudi',
+    album: 'Al-Quran',
+    artwork: 'https://i.ytimg.com/vi/r9VCQ_K2AFI/maxresdefault.jpg',
+    url: require('./assets/surah_yaseen.mp3'),
+  },
+  {
+    id: 1,
+    title: 'Surah Rahman',
+    artist: 'As-Sudais',
+    album: 'Al-Quran',
+    artwork:
+      'https://img.freepik.com/premium-vector/islamic-calligraphy-surah-al-rahman_764982-1992.jpg',
+    url: require('./assets/surah_rahman.mp3'),
+  },
+  {
+    id: 1,
+    title: 'Surah Mulk',
+    artist: 'Mishary Rashid Alafasay',
+    album: 'Al-Quran',
+    artwork: 'https://i.ytimg.com/vi/9Di2SFX22eY/maxresdefault.jpg',
+    url: require('./assets/surah_mulk.mp3'),
+  },
+  {
+    id: 1,
+    title: 'Surah Al-Waqia',
+    artist: 'Saad Al Gamidi',
+    album: 'Al-Quran',
+    artwork:
+      'https://learnqurankids.com/wp-content/uploads/2023/08/greeting-message-of-Selamat-Hari-Raya-Idul-Fitri-8.jpg',
+    url: require('./assets/surah_waqia.mp3'),
+  },
+  {
+    id: 1,
+    title: 'Surah Ad-Duha',
+    artist: 'Abdul Basit',
+    album: 'Al-Quran',
+    artwork: 'https://i.ytimg.com/vi/8yt8ZqGXG54/maxresdefault.jpg',
+    url: require('./assets/surah_adduha.mp3'),
+  },
+];
